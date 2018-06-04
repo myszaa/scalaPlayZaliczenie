40ce800ab629f1bbfe83a93d368a019ab53a5072
myszaa <michal2b@o2.pl>
2018-06-05 01:46:32 +0200

    unique user validation

--- scalaZaliczenie/app/model/User.scala
+++ scalaZaliczenie/app/model/User.scala
@@ -1,5 +1,6 @@
 package model
 
+import play.api.Logger
 import play.api.data.Form
 import play.api.data.Forms._
 
@@ -7,9 +8,17 @@ case class User(username : String, password : String, email: String)
 
 object User {
 
+  def uniqueUser(name: String) = {
+    var userDB = new UserDB
+    var userlist = userDB.findByName(name)
+    Logger.info("Ilosc userow: " + userlist.length.toString)
+    if(userlist.length > 0) false
+    else true
+  }
+
 
   val form = Form(mapping(
-    "username" -> nonEmptyText(5),
+    "username" -> nonEmptyText.verifying("User exists", username => uniqueUser(username)),
     "password" -> nonEmptyText.verifying("5 znakow w tym 1 specjalny i 1 wielka litera",
       name => name.matches("^(?=.*[A-Z])(?=.*[$@$!%*?&^])[A-Za-z\\d$@$!%^*?&]{5,}") ),
     "email" -> email
